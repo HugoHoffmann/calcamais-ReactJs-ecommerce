cebda72bc9c1900812056bc2cde265d788e26cbe
Hugo <hugohoffmann04@gmail.com>
2019-09-18 17:32:18 -0300

    new: global styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes/>
+      <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
