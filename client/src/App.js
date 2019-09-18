896f0e1bd0766a3d18fda60f4759a9cce35244c6
Hugo <hugohoffmann04@gmail.com>
2019-09-18 17:09:16 -0300

    new: config routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes/>
+    </BrowserRouter>
   );
 }
 
