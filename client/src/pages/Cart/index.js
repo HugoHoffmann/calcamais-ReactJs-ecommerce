0a022b851a5f448a9401d9c4dfe35df1cf3c3ef0
Hugo <hugohoffmann04@gmail.com>
2019-09-19 09:44:24 -0300

    new: style in Cart

--- client/src/pages/Cart/index.js
+++ client/src/pages/Cart/index.js
@@ -1,9 +1,61 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <div />
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt=""/>
+            </td>
+            <td>
+              <strong>Tênis para corriga</strong>
+              <span>R$150,00</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value="2" />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 300,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1"/>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">
+          Finalizar pedido
+        </button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1000,00</strong>
+        </Total>
+      </footer>
+    </Container>   
   );
 }
