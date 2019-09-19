b8cd992c4781499692332af62d7dce2f9e4a356b
Hugo <hugohoffmann04@gmail.com>
2019-09-19 08:43:02 -0300

    new: style in Home

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,9 +1,70 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md'
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt=""/>
+        <strong>Tênis para corrida</strong>
+        <span>R$ 150,00</span>
+        <button type="button" >
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span>Adiconar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt=""/>
+        <strong>Tênis para corrida</strong>
+        <span>R$ 150,00</span>
+        <button type="button" >
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span>Adiconar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt=""/>
+        <strong>Tênis para corrida</strong>
+        <span>R$ 150,00</span>
+        <button type="button" >
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span>Adiconar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt=""/>
+        <strong>Tênis para corrida</strong>
+        <span>R$ 150,00</span>
+        <button type="button" >
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span>Adiconar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt=""/>
+        <strong>Tênis para corrida</strong>
+        <span>R$ 150,00</span>
+        <button type="button" >
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+
+          <span>Adiconar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
