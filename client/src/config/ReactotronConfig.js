2b7a5fa9ee5660a6680804eb81937a219b8de06b
Hugo <hugohoffmann04@gmail.com>
2019-09-19 11:33:39 -0300

    new: config reactotron + redux

---
+++ client/src/config/ReactotronConfig.js
@@ -0,0 +1,15 @@
+// yarn add reactotron-react-js reactotron-redux
+
+import Reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+
+if (process.env.NODE_ENV === 'development') {
+  const tron = Reactotron
+    .configure()
+    .use(reactotronRedux())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
